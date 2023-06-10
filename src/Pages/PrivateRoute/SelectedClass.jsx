db310ece59eefb37ade9c0f4af099da0fea6e904
Dipto Das <ddipto396@gmail.com>
2023-06-10 23:46:15 +0600

    Classes page activated and selectedClass section implemented

--- src/Pages/PrivateRoute/SelectedClass.jsx
+++ src/Pages/PrivateRoute/SelectedClass.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { AuthContext } from '../../ContextProvider/AuthProvider'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import axios from 'axios';
+import {AiOutlineDollarCircle} from 'react-icons/ai'
+import {RiDeleteBin6Line} from 'react-icons/ri'
 
 const SelectedClass = () => {
+  const {user} = useContext(AuthContext)
+  const { refetch, data: selectClasses = [] } = useQuery({
+    queryKey: ['selectClasses'],
+    queryFn: async () => {
+        const res = await axios.get(`http://localhost:5000/selectClasses?studentEmail=${user?.email}`)
+        return res.data;
+    },
+})
   return (
-    <div>SelectedClass</div>
+      selectClasses && <div className="overflow-x-auto ms-4 mt-8">
+              <table className="table table-xs">
+                <thead className='table-head'>
+                  <tr >
+                    <th></th> 
+                    <th>Picture</th> 
+                    <th>Class Name</th> 
+                    <th>Instructor Name</th> 
+                    <th>Price</th> 
+                    <th>Buttons</th> 
+                  </tr>
+                </thead> 
+                <tbody>
+                  {
+                   selectClasses?.map((item, index)=>(
+                      <tr className='hover' key={item._id}>
+                        <th>{index + 1}</th>
+                        <td><img className='w-[70px]' src={item.picture} alt={item.className} /></td>
+                        <td>{item.className}</td>
+                        <td>{item.instructorName.toUpperCase()}</td>
+                        <td>{item.price}</td>
+                        <td>
+                        <button className="btn btn-outline btn-primary me-5">
+                           <RiDeleteBin6Line fontSize={20}/>
+                        </button>
+                        <Link to={`../payment/${item._id}`}>
+                            <button className="btn btn-outline btn-info">
+                                <AiOutlineDollarCircle fontSize={20}/>
+                            </button>
+                        </Link>
+                        </td>
+                      </tr>
+                    ))
+                  }
+              
+                </tbody> 
+                
+              </table>
+      </div>
+    
   )
 }
 
--- src/Pages/PublicRoute/Classes.jsx
+++ src/Pages/PublicRoute/Classes.jsx
@@ -3,9 +3,13 @@ import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios';
 import { AuthContext } from '../../ContextProvider/AuthProvider'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Swal from 'sweetalert2'
 
 const Classes = () => {
   const {user} = useContext(AuthContext)
+  const navigate = useNavigate();
   const { refetch, data: approvedClasses = [] } = useQuery({
     queryKey: ['approvedClasses'],
     queryFn: async () => {
@@ -13,10 +17,54 @@ const Classes = () => {
         return res.data;
     },
 })
+const notify = () => toast.warn("You have to login first", {autoClose:700});
+const handleSelect =(item) =>{
+    if(user){
+        const {picture, className, instructorName, price} = item;
+        const selectedClassInfo = {
+          picture,
+          className,
+          instructorName,
+          price,
+          addedBy:user?.email
+        }
+        fetch('http://localhost:5000/selectedClasses', {
+          method: 'POST',
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify(selectedClassInfo)
+        })
+        .then(res => res.json())
+        .then(data => {
+          if (data.insertedId) {
+              // reset();
+              Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Added to Select-cart',
+                showConfirmButton: false,
+                timer: 1500
+              });
+              setTimeout(()=>{
+                navigate('/StudentDash/selected');
+              },1600)
+                  
+              }
+          })
+        .catch(error => console.log(error))
+    }
+    else{
+      notify();
+      setTimeout(()=>{
+        navigate('/login')
+      },800)
+    }
+}
   return (
    
       approvedClasses && <div>
-       <h1 className='mt-8 text-center text-4xl font-semibold'>All Approved Classes</h1>
+       <h1 className='mt-8 text-center text-4xl font-bold'>All Approved Classes</h1>
       <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-3 gap-12">{
         approvedClasses?.map((item, index)=>(
           <div key={item._id} className="card mt-10 md:mt-16 bg-base-100 shadow-xl">
@@ -27,13 +75,14 @@ const Classes = () => {
             <h4>Available Seats: {item.seats}</h4>
             <h4>Price: {item.price}</h4>
             <div className="card-actions justify-start">
-              <button className="btn btn-primary">Select</button>
+              <button onClick={()=>handleSelect(item)} className="btn btn-primary">Select</button>
             </div>
           </div>
         </div>
         ))
       }
       </div>
+      <ToastContainer />
     </div>
   )
 }
