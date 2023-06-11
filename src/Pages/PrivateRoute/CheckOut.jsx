3f1d419e4bf347beb482b2db5c7ca77ecdeac738
Dipto Das <ddipto396@gmail.com>
2023-06-11 14:35:24 +0600

    payment method implemented

---
+++ src/Pages/PrivateRoute/CheckOut.jsx
@@ -0,0 +1,165 @@
+import React, {useContext, useEffect, useState} from 'react'
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { AuthContext } from '../../ContextProvider/AuthProvider'
+
+const CheckOut = ({item}) => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const { user } = useContext(AuthContext);
+    const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [transactionId, setTransactionId] = useState('');
+    
+    // console.log(item);
+    const {addedBy, picture, price, className, instructorName} = item;
+
+    useEffect(() => {
+        if (price > 0) {
+          fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ price }),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error('Failed to fetch client secret');
+              }
+              return response.json();
+            })
+            .then((data) => {
+              console.log(data.clientSecret);
+              setClientSecret(data.clientSecret);
+              console.log(clientSecret);
+            })
+            .catch((error) => {
+              console.error('Fetch client secret error:', error);
+              // Handle error state
+            });
+        }
+      }, []);
+    
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+      
+        if (!stripe || !elements) {
+          return;
+        }
+      
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+          return;
+        }
+      
+        const { error: paymentMethodError, paymentMethod } = await stripe.createPaymentMethod({
+          type: 'card',
+          card,
+        });
+      
+        if (paymentMethodError) {
+          console.log('Payment method error:', paymentMethodError);
+          setCardError(paymentMethodError.message);
+          return;
+        } else {
+          setCardError('');
+        }
+      
+        setProcessing(true);
+      
+        try {
+          const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card,
+                billing_details: {
+                  email: user?.email || 'unknown',
+                  name: user?.displayName || 'anonymous',
+                },
+              },
+            }
+          );
+      
+          if (confirmError) {
+            console.log('Payment confirmation error:', confirmError);
+          }
+      
+          console.log('Payment intent:', paymentIntent);
+          setProcessing(false);
+      
+          if (paymentIntent.status === 'succeeded') {
+            setTransactionId(paymentIntent.id);
+      
+            const paymentInfo = {
+              payee: addedBy,
+              email: user?.email,
+              transactionId: paymentIntent.id,
+              class:className,
+              price,
+              date: new Date(),
+            };
+      
+            fetch('http://localhost:5000/payments', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(paymentInfo),
+            })
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error('Failed to post payment');
+                }
+                return response.json();
+              })
+              .then((data) => {
+                console.log(data);
+                if (data.insertedId) {
+                    console.log('successful')
+                  // Display confirmation message
+                }
+              })
+              .catch((error) => {
+                console.error('Post payment error:', error);
+                // Handle error state
+              });
+          }
+        } catch (error) {
+          console.error('Confirm card payment error:', error);
+          // Handle error state
+        }
+      };
+
+  return (
+    <>
+        <form className="w-2/3 m-8" onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className="btn btn-primary btn-sm mt-4" type="submit" disabled={!stripe || !clientSecret || processing}>
+                Pay
+            </button>
+        </form>
+        {cardError && <p className="text-red-600 ml-8">{cardError}</p>}
+        {transactionId && <p className="text-green-500">Transaction complete with transactionId: {transactionId}</p>}
+   </>
+  )
+}
+
+export default CheckOut
--- src/Pages/PrivateRoute/Payment.jsx
+++ src/Pages/PrivateRoute/Payment.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOut from '../PrivateRoute/CheckOut'
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../ContextProvider/AuthProvider'
 
+// TODO: provide publishable Key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
 const Payment = () => {
+  const [item, setItem] = useState();
+  const {user} = useContext(AuthContext)
+  const params = useParams();
+  // console.log(params);
+  useEffect(()=>{
+    fetch(`http://localhost:5000/payClass/${params.id}`)
+    .then((res)=>res.json())
+    .then((data)=> setItem(data))
+    .catch((err)=> console.log(err.message))
+  },[])
+
   return (
-    <div>Payment</div>
+  item &&  <div>
+       <Elements stripe={stripePromise}>
+           <CheckOut item={item}></CheckOut>
+       </Elements>
+    </div>
   )
 }
 
